2ca479b798e5f177deeeaf74f5d7a3dfccb6e231
NickBryan26 <nblopezr@pucese.edu.ec>
2021-07-08 19:15:10 -0500

    deber

---
+++ cmake-build-debug/CMakeFiles/trabajoenclase.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trabajoenclase.dir/main.cpp.obj"
+  "libtrabajoenclase.dll.a"
+  "trabajoenclase.exe"
+  "trabajoenclase.exe.manifest"
+  "trabajoenclase.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trabajoenclase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/trabajoenclase.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for trabajoenclase.
+# This may be replaced when dependencies are built.
